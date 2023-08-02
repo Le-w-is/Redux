c0aef2fa8a12243c505698c46f7ea01985c25f5f
Lewis <100314560+Le-w-is@users.noreply.github.com>
2023-08-02 13:41:06 +0100

    added reactions to posts

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux/es/hooks/useSelector";
 import { selectAllPosts } from "./postsSlice";
 import PostAuthor from "./postAuthor";
 import TimeAgo from "./TimeAgo";
+import ReactionButtons from "./ReactionButtons";
 
 const PostsList = () => {
     const posts = useSelector(selectAllPosts)
@@ -15,8 +16,8 @@ const PostsList = () => {
             <p classname='postCredit'>
                 <PostAuthor userId={post.userId} />
                 <TimeAgo timestamp={post.date} />
-
             </p>
+            <ReactionButtons post={post} />
         </article>
     ))
 
